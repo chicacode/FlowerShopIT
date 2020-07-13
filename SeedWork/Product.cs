f182c3043ddea3571bcba41763df3c22c62503e7
gerihal <gerahernan.al@gmail.com>
2020-07-13 11:28:04 +0200

    added TODO and UML

--- SeedWork/Product.cs
+++ SeedWork/Product.cs
@@ -13,5 +13,7 @@ namespace FlowerShopIT.Models
         // 1 Floristeria, 2 ARBOL, 3 FLORES, 4 Decoración que consta de Materiales de madera o de plastico. 
         // propiedad general. Todos los productos tienen un precio
         // crear menu donde el user esoga si desea crear floristeria, añadir productos o ver el stock. 
+        // crear un diagrama de flujo de gen my model
+        // crear carpeta controllar que se encargara de metodos de añadir elementos a listas
     }
 }
