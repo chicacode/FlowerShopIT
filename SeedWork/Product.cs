15439ca6ebbbdf0f29c9418d81b7ab7286c46ec4
gerihal <gerahernan.al@gmail.com>
2020-07-13 13:26:47 +0200

    added properties in product class

--- SeedWork/Product.cs
+++ SeedWork/Product.cs
@@ -6,14 +6,33 @@ using System.Threading.Tasks;
 
 namespace FlowerShopIT.Models
 {
-    class Product
-    {
-        // TODO
+         // TODO
         // Clase madre del cual heredan los productos que se venden en floristeria
         // 1 Floristeria, 2 ARBOL, 3 FLORES, 4 Decoración que consta de Materiales de madera o de plastico. 
         // propiedad general. Todos los productos tienen un precio
         // crear menu donde el user esoga si desea crear floristeria, añadir productos o ver el stock. 
         // crear un diagrama de flujo de gen my model
         // crear carpeta controllar que se encargara de metodos de añadir elementos a listas
+
+    class Product
+    {
+        // attributes
+        private double price;
+
+        public Product( double price ){
+
+            this.price = price;
+        }
+
+        public double PRICE { 
+
+            get { return this.price; }
+            set { this.price = setPrice(value); }
+        }
+
+        private double setPrice(double _price ){
+
+            return this.price = _price; 
+        }
     }
 }
