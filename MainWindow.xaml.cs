d13aaccc3ab9163a86e35730fcce2f4f25d6a2ac
gerihal <gerahernan.al@gmail.com>
2020-07-16 12:31:29 +0200

    added structure with grid to save data in method

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -29,23 +29,39 @@ namespace FlowerShopIT
         private void ButtonTree_Click ( object sender, RoutedEventArgs e )
         {
             // show texbox and retrieve data
-           
-               
+            stack1.Visibility = Visibility;               
         }
 
         private void ButtonFlower_Click ( object sender, RoutedEventArgs e )
         {
-
+            stack2.Visibility = Visibility;
         }
 
         private void ButtonDeco_Click ( object sender, RoutedEventArgs e )
         {
-
+            stack3.Visibility = Visibility;
         }
 
         private void ViewStock_Click ( object sender, RoutedEventArgs e )
         {
 
+        }
+
+
+
+        private void AddTree_Click ( object sender, RoutedEventArgs e )
+        {
+            // Recoge INFO y se crea el OBJETO TREE
+        }
+
+        private void AddDeco_Click ( object sender, RoutedEventArgs e )
+        {
+            // Recoge INFO y se crea el OBJETO DECO
+        }
+
+        private void AddFlower_Click ( object sender, RoutedEventArgs e )
+        {
+            // Recoge INFO y se crea el OBJETO FLOWER
         }
     }
 }
